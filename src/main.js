22827e57491338735e1c577152c0f8c83d51f7d8
Eric Liu <ericyl.us@gmail.com>
2024-11-23 09:29:15 -0800

    Upgrade dependencies to latest

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import App from './App.svelte';
+import { mount } from "svelte";
+import App from "./App.svelte";
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+mount(App, {
+  target: document.body,
+  props: {
+    name: "world",
+  },
 });
-
-export default app;
